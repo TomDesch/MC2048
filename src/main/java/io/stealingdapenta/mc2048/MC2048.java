c8a28f0652f294fd13cd277198b5b1a4f1fdac88
StealingDaPenta <tomdescheemaeker@hotmail.com>
2023-12-30 20:36:09 +0100

    NumberRepresentations

--- src/main/java/io/stealingdapenta/mc2048/MC2048.java
+++ src/main/java/io/stealingdapenta/mc2048/MC2048.java
@@ -1,8 +1,33 @@
 package io.stealingdapenta.mc2048;
 
-public class MC2048 {
+import java.util.logging.Logger;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class MC2048 extends JavaPlugin {
+
+    private static final String PLUGIN_ENABLED = "MC2048 plugin enabled.";
+    private static final String PLUGIN_DISABLED = "MC2048 plugin disabled.";
+    public static Logger logger;
+    private static MC2048 instance = null;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        logger = this.getLogger();
+
+//        Objects.requireNonNull(this.getCommand("consoleclear"))
+//               .setExecutor(consoleClearCommand);
+
+        logger.info(ChatColor.GOLD + PLUGIN_ENABLED);
+    }
+
+    @Override
+    public void onDisable() {
+        logger.info(ChatColor.GOLD + PLUGIN_DISABLED);
+    }
+
+    public static MC2048 getInstance() {
+        return instance;
     }
 }
--- src/main/java/io/stealingdapenta/mc2048/utils/NumberRepresentations.java
+++ src/main/java/io/stealingdapenta/mc2048/utils/NumberRepresentations.java
@@ -1,2 +1,41 @@
-package io.stealingdapenta.mc2048.utils;public class NumberRepresentations {
+package io.stealingdapenta.mc2048.utils;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public enum NumberRepresentations {
+    TWO(2, Material.BONE_BLOCK),
+    FOUR(4, Material.HONEY_BLOCK),
+    EIGHT(8, Material.HONEYCOMB_BLOCK),
+    SIXTEEN(16, Material.HORN_CORAL_BLOCK),
+    THIRTY_TWO(32, Material.RAW_GOLD_BLOCK),
+    SIXTY_FOUR(64, Material.GOLD_BLOCK),
+    HUNDRED_TWENTY_EIGHT(128, Material.BONE_BLOCK),
+    TWO_HUNDRED_FIFTY_SIX(256, Material.OBSIDIAN),
+    FIVE_HUNDRED_TWELVE(512, Material.NETHER_BRICK),
+    ONE_THOUSAND_TWENTY_FOUR(1024, Material.NETHERITE_BLOCK),
+    TWO_THOUSAND_FORTY_EIGHT(2048, Material.DIAMOND_BLOCK),
+    FOUR_THOUSAND_NINETY_SIX(4096, Material.EMERALD_BLOCK),
+    EIGHT_THOUSAND_ONE_HUNDRED_NINETY_TWO(8192, Material.DIAMOND_BLOCK),
+    SIXTEEN_THOUSAND_THREE_HUNDRED_EIGHTY_FOUR(16384, Material.END_STONE);
+
+    private final int number;
+    private final Material representation;
+
+    NumberRepresentations(int number, Material representation) {
+        this.number = number;
+        this.representation = representation;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Material getRepresentation() {
+        return representation;
+    }
+
+    public ItemStack getDisplayableBlock() {
+        return new ItemStack(representation, number);
+    }
 }
